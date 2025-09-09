ca1601ff52efa4e622aa6e8728e8a97ea97b6d18
tc-fcopy <tc_fcopy@163.com>
2025-09-09 19:52:21 +0800

    add template

--- README.md
+++ README.md
@@ -9,3 +9,10 @@ go get github.com/cloudwego/eino/schema
 go get github.com/cloudwego/eino/components/model
 
 go get github.com/cloudwego/eino-ext/components/model/ark
+
+
+stage2: 认识ChatTemplate组件
+
+使用一个模板来进行与LLM对话
+
+go get github.com/cloudwego/eino/components/prompt
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"github.com/cloudwego/eino-ext/components/model/ark"
+	"github.com/cloudwego/eino/components/prompt"
 	"github.com/cloudwego/eino/schema"
 	"github.com/joho/godotenv"
 	"os"
@@ -21,29 +22,44 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	input := []*schema.Message{
-		schema.SystemMessage("你是美国总统特朗普"),
-		schema.UserMessage("你好,你叫什么名字"),
+	//input := []*schema.Message{
+	//	schema.SystemMessage("你是美国总统特朗普"),
+	//	schema.UserMessage("你好,你叫什么名字"),
+	//}
+
+	template := prompt.FromMessages(schema.FString,
+		schema.SystemMessage("你是一个{role}"),
+		&schema.Message{
+			Role:    schema.User,
+			Content: "请帮帮我，我想解决{task}",
+		},
+	)
+
+	params := map[string]any{
+		"role": "美国总统川普",
+		"task": "帮我计算一下美国对日本关税增加了多少",
 	}
 
-	//response, err := model.Generate(ctx, input)
-	//if err != nil {
-	//	panic(err)
-	//}
-	//
-	//println(response.Content)
+	messages, err := template.Format(ctx, params)
 
-	reader, err := model.Stream(ctx, input)
+	response, err := model.Generate(ctx, messages)
 	if err != nil {
 		panic(err)
 	}
-	defer reader.Close()
-	for {
-		chunk, err := reader.Recv()
-		if err != nil {
-			break
-		}
-		print(chunk.Content)
-	}
+
+	println(response.Content)
+
+	//reader, err := model.Stream(ctx, input)
+	//if err != nil {
+	//	panic(err)
+	//}
+	//defer reader.Close()
+	//for {
+	//	chunk, err := reader.Recv()
+	//	if err != nil {
+	//		break
+	//	}
+	//	print(chunk.Content)
+	//}
 
 }
